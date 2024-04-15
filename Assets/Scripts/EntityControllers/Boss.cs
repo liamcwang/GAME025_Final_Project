4fd2f36ef93cf010ccdf69969b71d31976d13916
liamwang <liamcwang@gmail.com>
2024-04-15 14:35:39 -0400

    Attempting to make boss shoot projectiles at player

--- Assets/Scripts/EntityControllers/Boss.cs
+++ Assets/Scripts/EntityControllers/Boss.cs
@@ -6,7 +6,7 @@ using System;
 
 public class Boss : MonoBehaviour
 {
-    public enum Methods {LERP_MOVE, WAIT};
+    public enum Methods {LERP_MOVE, WAIT, SHOOT_PLAYER};
 
     public ActionPattern[] actionPatterns;
     private float waitTimer;
@@ -14,20 +14,28 @@ public class Boss : MonoBehaviour
     private Vector2 destVector = Vector2.zero;
     private Vector3 heading = Vector3.zero;
     private Rigidbody2D rb;
-
+    private ProjectileSpawner projectileSpawner;
     private MethodInfo currentMethod;
-    private object[] currentMethodArgs;
+    private int currentMethodPointer;
+    private Player playerRef;
+    private object[][] currentMethodArgs = new object[Enum.GetNames(typeof(Methods)).Length][];
 
+    private void Awake()
+    {
+
+    }
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        projectileSpawner = GetComponent<ProjectileSpawner>();
+        playerRef = GameManager.Instance.Player;
         interpretCall(actionPatterns[actionPointer].methodCall);
     }
 
     private void Update()
     {
-        bool result = (bool) currentMethod.Invoke(this, currentMethodArgs);
+        bool result = (bool) currentMethod.Invoke(this, currentMethodArgs[currentMethodPointer]);
         if (result) {
             actionPointer = (actionPointer + 1) % actionPatterns.Length;
             interpretCall(actionPatterns[actionPointer].methodCall);
@@ -35,11 +43,10 @@ public class Boss : MonoBehaviour
     }
 
     private void interpretCall(MethodCall methodCall) {
-        MethodInfo mi;
-
+        // MethodInfo methodInfo;
+        object[] args = new object[methodCall.arguments.Length];
         switch(methodCall.method) {
             case Methods.LERP_MOVE:
-                object[] args = new object[methodCall.arguments.Length];
                 for (int i = 0; i < methodCall.arguments.Length; i++) {
                     args[i] = Convert.ToSingle(methodCall.arguments[i]);
                 }
@@ -47,12 +54,24 @@ public class Boss : MonoBehaviour
                 destVector.x = (float) args[0];
                 destVector.y = (float) args[1];
                 currentMethod = this.GetType().GetMethod("MoveToPoint");
-                currentMethodArgs = new object[]{(float) args[2]};
+                currentMethodPointer = (int) Methods.LERP_MOVE;
+                currentMethodArgs[currentMethodPointer] = new object[]{args[2]}; // run from it, dread it, the GC comes for us all
                 break;
             case Methods.WAIT:
                 waitTimer = Convert.ToSingle(methodCall.arguments[0]);
                 currentMethod = this.GetType().GetMethod("Wait");
-                currentMethodArgs = new object[0];
+                currentMethodPointer = (int) Methods.WAIT;
+                break;
+            case Methods.SHOOT_PLAYER:
+
+
+                int numShots = (int) int.Parse(methodCall.arguments[0]);
+                float shootDelay = (float) Convert.ToSingle(methodCall.arguments[1]);
+                StartCoroutine(ShootAtPlayer(numShots, shootDelay));
+                // not my intended way of doing this, but I can't remember what my plan was
+                waitTimer = numShots * shootDelay; 
+                currentMethod = this.GetType().GetMethod("Wait");
+                currentMethodPointer = (int) Methods.WAIT;
                 break;
             default:
                 break;
@@ -61,7 +80,7 @@ public class Boss : MonoBehaviour
     }
 
     public bool MoveToPoint(float lerpFactor) {
-        transform.position = Vector2.Lerp(transform.position, destVector, Time.deltaTime * lerpFactor);
+        rb.MovePosition(Vector2.Lerp(transform.position, destVector, Time.deltaTime * lerpFactor));
 
         heading.x = (destVector.x - transform.position.x);
         heading.y = (destVector.y - transform.position.y);
@@ -69,13 +88,33 @@ public class Boss : MonoBehaviour
         float distanceSquared = heading.x * heading.x + heading.y * heading.y;
         float distance = Mathf.Sqrt(distanceSquared);
         
-        return distance <= 0.1;
+        return distance <= 0.1f;
     }
 
     public bool Wait() {
         waitTimer -= Time.deltaTime;
-        Debug.Log($"{waitTimer}");
-        return waitTimer <= 0.01;
+        return waitTimer <= 0.01f;
+    }
+
+    IEnumerator ShootAtPlayer(int numShots, float shootDelay) {
+        Vector3 aimVector = Vector3.zero;
+        Quaternion aimRotation = Quaternion.identity;
+        Vector2 heading = Vector2.zero;
+
+
+        for (int i = 0; i< numShots; i++) {
+            heading.x = playerRef.transform.position.x - transform.position.x;
+            heading.y = playerRef.transform.position.y - transform.position.y;
+
+            aimVector.z = (float) (Mathf.Atan2(heading.y, heading.x) * Mathf.Rad2Deg);
+            Debug.Log($"{aimVector.z}");
+
+            aimRotation.eulerAngles = aimVector;
+            Debug.Log($"{aimRotation.eulerAngles}");
+            projectileSpawner.spawnProjectile(transform.position, aimRotation);
+
+            yield return new WaitForSeconds(shootDelay);
+        }
     }
 
 }
