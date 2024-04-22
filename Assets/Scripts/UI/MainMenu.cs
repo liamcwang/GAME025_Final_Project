f04846c4e565e531a31de36d3ee210a312d92a51
liamwang <liamcwang@gmail.com>
2024-04-22 17:07:06 -0400

    added credits
    - main menu controls showing and closing credits

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour
 {
     CanvasView canvasView;
+    bool creditsEnabled = false;
+
+    // didn't think about this, but if I want a screen to be easily open and closeable at the press of a button, it requires me to add an update loop checking for inputs. This isn't great imo.
+
+    private void Update()
+    {
+        if (Input.anyKeyDown && creditsEnabled) {
+            canvasView.HideUIElement("Credits");
+            creditsEnabled = false;
+        }
+    }
 
     private void Start()
     {
@@ -16,7 +27,8 @@ public class MainMenu : MonoBehaviour
     }
 
     public void Credits() {
-        GameManager.Credits();
+        canvasView.ShowUIElement("Credits");
+        creditsEnabled = true;
     }
 
     public void QuitGame() {
